668ddfdf846071ce36b4c3761b951376fbfebe6d
Mason Kleinsorge <kleinsorgem97@gmail.com>
2023-10-19 13:20:48 -0500

    copying sample

---
+++ sample_copy/src/main.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata";
+import * as _path from 'path';
+import { SHAMAN_API_TYPES, ShamanExpressApp, ShamanExpressController } from 'shaman-api';
+import { HealthController } from './controllers/health.controller';
+import { ProxyWidgetModule } from "./widgets/proxy-widget/proxy-widget.module";
+import { SampleService } from "./sample.service";
+
+let bootstrap = async () => {
+  let configPath = _path.join(__dirname, '..', 'app', 'config.json');
+  const app = new ShamanExpressApp({configPath: configPath, port: 5000});
+  let container = await app.compose();
+  container.bind<SampleService>("SampleService").to(SampleService);
+  container.bind<ShamanExpressController>(SHAMAN_API_TYPES.ApiController).to(HealthController);
+  app.configureRouter([new ProxyWidgetModule()]);
+  await app.startApplication();
+}
+
+bootstrap().catch(ex => {
+  console.error(ex);
+  process.exit(1);
+});
---
+++ sample_copy/src/widgets/proxy-widget/proxy-widget.types.ts
@@ -0,0 +1,5 @@
+const PROXY_WIDGET_TYPES = {
+  ProxyWidgetService: "ProxyWidgetService"
+};
+
+export { PROXY_WIDGET_TYPES };
