815680d91b4378f2af1e2678371200b8afe90a2f
Mason Kleinsorge <Kleinsorgem97@outlook.com>
2023-10-19 14:23:52 -0500

    removing unused function

--- library/src/modules/auth/functions/password.functions.ts
+++ library/src/modules/auth/functions/password.functions.ts
@@ -1,9 +1,5 @@
 import * as _bcrypt from 'bcryptjs';
 
-export function generatePasswordHash(password: string): string {
-  return _bcrypt.hashSync(password, _bcrypt.genSaltSync(8));
-}
-
 export function comparePasswordToHash(password: string, passwordHash: string): boolean {
   return _bcrypt.compareSync(password, passwordHash);
 }
