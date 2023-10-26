9ae1beae9841864d352cd445775b332768716cda
Mason Kleinsorge <kleinsorgem97@gmail.com>
2023-10-26 15:15:57 -0500

    minor refactor

--- sample/src/main.ts
+++ sample/src/main.ts
@@ -1,12 +1,19 @@
 import * as _path from 'path';
 import "reflect-metadata";
-import { IJsonService, JsonFileService, SHAMAN_API_TYPES, ShamanAuthModule, ShamanExpressApp, ShamanExpressController } from 'shaman-api';
+import {
+  IJsonService,
+  JsonFileService,
+  SHAMAN_API_TYPES,
+  ShamanAuthModule,
+  ShamanExpressApp,
+  ShamanExpressController
+} from 'shaman-api';
 import { HealthController } from './controllers/health.controller';
 import { UserController } from './controllers/user.controller';
 import { SampleService } from './sample.service';
+import { SAMPLE_TYPES } from './sample.types';
 import { UserService } from './services/user.service';
 import { ProxyWidgetModule } from "./widgets/proxy-widget/proxy-widget.module";
-import { SAMPLE_TYPES } from './sample.types';
 
 let bootstrap = async () => {
   let configPath = _path.join(__dirname, '..', 'app', 'config.json');
@@ -17,7 +24,7 @@ let bootstrap = async () => {
   container.bind<IJsonService>(SHAMAN_API_TYPES.ApiService).to(JsonFileService);
   container.bind<SampleService>(SAMPLE_TYPES.SampleService).to(SampleService);
   container.bind<UserService>(SAMPLE_TYPES.UserDao).to(UserService);
-  
+
   // compose controllers
   container.bind<ShamanExpressController>(SHAMAN_API_TYPES.ApiController).to(HealthController);
   container.bind<ShamanExpressController>(SHAMAN_API_TYPES.ApiController).to(UserController);
