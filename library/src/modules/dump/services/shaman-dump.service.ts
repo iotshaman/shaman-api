c3be86603ef1d355dc7952bb3f23c286cede03e7
Mason Kleinsorge <Kleinsorgem97@outlook.com>
2023-11-15 16:57:28 -0600

    add check to ensure dump file exists

--- library/src/modules/dump/services/shaman-dump.service.ts
+++ library/src/modules/dump/services/shaman-dump.service.ts
@@ -4,6 +4,7 @@ import { IDumpService } from "./dump-services/dump-service.interface";
 import { JsonRepoDumpService } from "./dump-services/json-repo-dump.service";
 import { MysqlDumpService } from "./dump-services/mysql-dump.service";
 import { SqliteDumpService } from "./dump-services/sqlite-dump.service";
+import * as _fs from 'fs';
 
 export interface IShamanDumpService {
   getDump: (dbConfig: DatabaseConfig) => Promise<string>;
@@ -23,7 +24,12 @@ export class ShamanDumpService implements IShamanDumpService {
   getDump = (dbConfig: DatabaseConfig): Promise<string> => {
     let dumpService = this.dumpServiceFactory.find(ds => ds.type === dbConfig.type);
     if (!dumpService) return Promise.reject(new Error(`Dump service '${dbConfig.type}' not found.`));
-    return dumpService.getDump(dbConfig);
+    return dumpService.getDump(dbConfig)
+      .then(dump => {
+        if (!_fs.existsSync(dump))
+          throw new Error(`Dump file '${dump}' not found.`);
+        return dump;
+      });
   };
 
 }
